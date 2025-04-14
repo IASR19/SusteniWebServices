a76487111a70e7ac3ebd2825e8535ba825eaf06f
Itamar <itamar_augusto@ges.inatel.br>
2025-04-14 11:17:02 +0200

    :sparkle: start Autotune priorities

--- SusteniWebServices/obj/Debug/net8.0/SusteniWebServices.AssemblyInfo.cs
+++ SusteniWebServices/obj/Debug/net8.0/SusteniWebServices.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SusteniWebServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97cabc57c2615e3c2c43f7b193b71371cdd398d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aeb99fe6069b3c33ec5a19d55a9cd1e77fd3e95")]
 [assembly: System.Reflection.AssemblyProductAttribute("SusteniWebServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SusteniWebServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
